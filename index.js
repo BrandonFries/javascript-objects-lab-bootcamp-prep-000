162ea18cc5559ff110e5d04e1034739b0656e483
Brandon Fries <brandonfries1993@gmail.com>
2019-06-06 20:46:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,14 +4,17 @@ var obj = { prop: 1 }
 function updateObjectWithKeyAndValue(object, key, value) {
   return Object.assign({},obj,{[key]:value})
 }
+
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   object[key] = value
   return object
 }
+
 function deleteFromObjectByKey(object, key) {
   delete object.key;
   return obj
 }
+
 function destructivelyDeleteFromObjectByKey(object, key) {
   delete object.key
   return object
