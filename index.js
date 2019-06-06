6f16f897c08c44342b14c0652609b482a9519c91
Brandon Fries <brandonfries1993@gmail.com>
2019-06-06 20:47:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 }
 
 function deleteFromObjectByKey(object, key) {
-  delete object.1;
+  delete object.prop: 1;
   return obj
 }
 
